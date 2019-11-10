9aa5e55cf47408d5715c32eff1b9e25bf8a9ccca
Chloé Vieille <chloevieille@hotmail.com>
2019-11-10 16:47:26 +0100

    Amélioration affichage des commentaires

--- app/resources/views/comments/commentsDisplay.blade.php
+++ app/resources/views/comments/commentsDisplay.blade.php
@@ -1,8 +1,21 @@
 
 @foreach($comments as $comment)
-<div class="display-comment" @if($comment->product_id != null) style="margin-left:40px;" @endif>
-    <strong>{{ $comment->user->name }}</strong>
-    <p>{{ $comment->content }}</p>
-    <a href="" id="reply"></a>
-</div>
+<hr/>
+<div class="review-block">
+    <div class="row">
+        <div class="col-sm-3">
+            <div class="review-block-name"><a href="#">{{$comment->user->name}}</a></div>
+            <div class="review-block-date">{{ $comment->created_at->format('d M Y')}}<br/>{{ $comment->created_at->format('H:i')}}</div>
+        </div>
+        <div class="col-sm-9">
+            <div class="review-block-rate">
+                @for ($i = 0; $i < 5; $i++)
+                <i class="fa fa-star{{ $comment->mark <=$i?'-o':'' }}" style="color: orange; font-size: 20px" aria-hidden="true"></i>
+
+                @endfor
+            </div>
+            <div class="review-block-title"><strong>{{$comment->title}}</strong></div>
+            <div class="review-block-description">{{$comment->content}}</div>
+        </div>
+    </div>
 @endforeach
